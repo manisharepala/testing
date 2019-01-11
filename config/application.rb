5e578ba06baf7cf6fb45222579170e4ee923c8a7
Inayat Hussain <peace2inayat@gmail.com>
2019-01-11 16:32:50 +0530

    assets precompile issue production environment_2

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module AssessmentApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
 
+    config.assets.prefix = "/assessment/assets"
+
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
