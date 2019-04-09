dd8c3161b6e2bf6a7e213ffdb3c24cdba69b71a7
inayat <peace.inayat@gmail.com>
2019-04-09 15:15:01 +0530

    multi chapter quiz post/get

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
   post '/assessment/quiz_attempt_data', to: 'application#quiz_attempt_data'
+  post '/assessment/multi_chapter_quiz_attempt_data', to: 'application#multi_chapter_quiz_attempt_data'
 
   get "/assessment/zip_upload_question" => "quizzes#zip_upload_question"
   post "/assessment/post_zip_upload_question" => "quizzes#post_zip_upload_question"
@@ -38,6 +39,7 @@ Rails.application.routes.draw do
   post '/assessment/get_assessments_active_duration', to: 'quizzes#get_assessments_active_duration'
 
   get '/assessment/get_quiz_attempt_data', to: 'quizzes#get_quiz_attempt_data'
+  get '/assessment/get_multi_chapter_quiz_attempt_data', to: 'quizzes#get_multi_chapter_quiz_attempt_data'
 
 #teacher web api's
   # get '/assessment/user_assessments_by_category', to: 'api#user_assessments_by_category'
