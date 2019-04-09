62939b15e554119bfaedafceecea451a2704ce8a
inayat <peace.inayat@gmail.com>
2019-04-09 12:44:01 +0530

    adding indexes

--- app/models/quiz_attempt_data.rb
+++ app/models/quiz_attempt_data.rb
@@ -4,8 +4,8 @@ class QuizAttemptData
   field :data, type: BSON::Binary
   field :user_id, type: String
 
-  # index({:user_id=>1})
-  # index({'data.asset_download_id' => 1,:user_id=>1})
-  # index({'data.asset_download_id' => 1,:user_id=>1,'data.player_subtype'=>1})
+  index({:user_id=>1})
+  index({'data.asset_download_id' => 1,:user_id=>1})
+  index({'data.asset_download_id' => 1,:user_id=>1,'data.player_subtype'=>1})
 
 end
