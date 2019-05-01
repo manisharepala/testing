1184295b9e616cc73e62a01575c7beda6c5b62f7
inayat <peace.inayat@gmail.com>
2019-05-02 00:02:19 +0530

    ck editor \r\n issue fix _ 2 for all block elements

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,14 +2,42 @@
  * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
-CKEDITOR.on( 'instanceReady', function( ev )
-{
-    ev.editor.dataProcessor.writer.setRules( 'p',
-        {
-            indent : false,
-            breakBeforeOpen : false,
-            breakAfterOpen : false,
-            breakBeforeClose : false,
-            breakAfterClose : false
-        });
+
+CKEDITOR.on('instanceReady', function(ev){
+    var el = [ "p", "div", "table", "tbody", "tr", "td", "h1", "h2", "h3", "h4", "h5", "h6", "ul", "ol", "pre", "address", "blockquote", "dl", "fieldset", "form", "hr", "noscript", "center"];
+    el.forEach(function(v) {
+        ev.editor.dataProcessor.writer.setRules(v,
+            {
+                indent: false,
+                breakBeforeOpen: false,
+                breakAfterOpen: false,
+                breakBeforeClose: false,
+                breakAfterClose: false
+            }
+        );
+    });
 });
+
+
+//CKEDITOR.on('instanceReady', function(ev){
+//    var dtd = CKEDITOR.dtd;
+//
+//    for ( var e in CKEDITOR.tools.extend( {},
+//        dtd.$block,
+//        dtd.$listItem,
+//        dtd.$tableContent )
+//        ) {
+//        ev.editor.dataProcessor.writer.setRules(v,
+//            {
+//                indent: false,
+//                breakBeforeOpen: false,
+//                breakAfterOpen: false,
+//                breakBeforeClose: false,
+//                breakAfterClose: false
+//            }
+//        );
+//    }
+//});
+
+
+
