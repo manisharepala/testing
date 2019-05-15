f5f8d7c2de8221d1548b902d9742f08e86a3fc62
inayat <peace.inayat@gmail.com>
2019-05-15 13:48:15 +0530

    imps

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,6 +7,12 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    @tags = {}
+    @tags['course'] = [['CBSE','CBSE'],['SSC','SSC']]
+    @tags['grade'] = [['CBSE','CBSE'],['SSC','SSC']]
+    @tags['subject'] = [['CBSE','CBSE'],['SSC','SSC']]
+    @tags['chapter'] = [['CBSE','CBSE'],['SSC','SSC']]
+    @tags['concept'] = [['CBSE','CBSE'],['SSC','SSC']]
   end
 
   def update
@@ -21,6 +27,7 @@ class QuestionsController < ApplicationController
         d = {}
         d['answer_english'] = Question.get_original_text(h['answer_english'])
         d['id'] = h['id']
+        d['fraction'] = h['fraction']
         qa_values << d
       end
       data = data.merge(question_answers_attributes:qa_values)
@@ -39,58 +46,6 @@ class QuestionsController < ApplicationController
 
   def set_question
     @question = Question.find(params[:id])
-  end
-
-
-  def send_question_images(images,question_params)
-    src_images = []
-    src_image_path = []
-    que = question_params['question_language_specific_datas_attributes'].values[0]['question_text']
-    gen = question_params['question_language_specific_datas_attributes'].values[0]['general_feedback']
-    Nokogiri::HTML(que).css('img').map{ |i| i['src'] }.each do |img|
-      src_images << img.split("/").last
-      src_image_path << img
-    end
-    Nokogiri::HTML(gen).css('img').map{ |i| i['src'] }.each do |img|
-      src_images << img.split("/").last
-      src_image_path << img
-    end
-
-    if params[:qtype] == 'SmcqQuestion' || params[:qtype] == 'MmcqQuestion' || params[:qtype] == 'TrueFalseQuestion' || params[:qtype] == 'McqMatrixQuestion' || params[:qtype] == 'AssertionReasonQuestion'
-      question_params['question_answers_attributes'].each do |qa|
-        Nokogiri::HTML(qa[1]['answer_english']).css('img').map{ |i| i['src'] }.each do |img|
-          src_images << img.split("/").last
-          src_image_path << img
-        end
-      end
-    end
-    image_ids = []
-    src_image_names = src_images.map{|n| n.downcase.split('.')[0]}
-    dir_path = Rails.root.to_s + "/public"
-    src_image_names.each do |src_image_name|
-      index = src_image_names.index(File.basename(src_image_name).split('.')[0].downcase)
-      img_name = (src_images[index]).split('.')[0] + ".jpg"
-      file_image= (src_image_path[index]).split('?').first
-      logger.info "---------------------------------------------------------------------file images----------------------------------------------------------------------------------------------------"
-      logger.info images[index]
-      if(images.count>0)
-        image= (images[index]).split('/').last.split('.')[0]
-      else
-        image =[]
-      end
-      logger.info "---------------------------------------------------------------------images----------------------------------------------------------------------------------------------------"
-      logger.info src_image_name
-      logger.info image
-      if(src_image_name !=image)
-        logger.info "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-        image_ids << (Image.create(name: img_name, key: file_image, file_path:(dir_path+file_image))).guid
-        logger.info "---------------------------------------------------------------------image_ids----------------------------------------------------------------------------------------------------"
-        logger.info image_ids
-      end
-    end
-    @question.image_ids = image_ids
-    @question.save!
-    @question.upload_images
   end
 
   private
