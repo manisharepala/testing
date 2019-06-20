ceca267f8ad8198197d92e453dd6d44260aa89ef
inayat <peace.inayat@gmail.com>
2019-06-20 16:46:51 +0530

    imps

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -9,7 +9,8 @@ class Api::V1::StudentsController < ApplicationController
       d = {}
       d['publish_id'] = qtg.id
       d['subject'] = 'Physics'
-      d['sections'] = qtg.group_ids.map{|g_id| (UserManagementServer.get_group_details(g_id,current_user.token)['name'] rescue 'Group not found')}
+      d['from'] = UserManagementServer.get_user_details(qtg.published_by,current_user.token)['name'] rescue ''
+      d['to'] = qtg.user_ids.map{|u_id| (UserManagementServer.get_user_details(u_id,current_user.token)['name'])} + qtg.group_ids.map{|g_id| (UserManagementServer.get_group_details(g_id,current_user.token)['name'])} rescue []
       d['published_on'] = qtg.published_on.to_i
       d['time_open'] = qtg.time_open
       d['time_close'] = qtg.time_close
@@ -54,6 +55,7 @@ class Api::V1::StudentsController < ApplicationController
     d['type'] = quiz.type
 
     concept_guids = JSON.parse(quiz.focus_area).map{|d| d['guid']} rescue []
+    concept_guids = ["fff3f076-7bd8-ae58-8e87-dd36ec97eb41", "30b445f5-5426-0850-5490-ac3fc5d87eb0", "13595318-955d-1146-81a6-23f6f3ec829e", "3a06991c-aa7e-c496-1562-446c3dea7656"] if !concept_guids.present?
     d['chapter_concepts'] = (ContentServer.get_concept_chapters(concept_guids,current_user.token).values rescue {})
     render json: d
 
