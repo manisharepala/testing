a0a331d4b24a3c5da01a953251806283c68cf8d7
dilip <dilip.bv@myedutor.com>
2019-07-29 15:13:59 +0530

    user get_all_assessment_attempts method updated in quizzes controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -838,22 +838,33 @@ class QuizzesController < ApplicationController
   end
 
   def get_all_assessment_attempts
-    result = []
-     data = QuizAttemptData.where("data.book_id"=>params[:book_id],:user_id=>current_user.id)
+    result_data = {}
+    attempts = []
+    assessments = []
+    a = {}
+    data = QuizAttemptData.where("data.book_id"=>params[:book_id],:user_id=>15664.id)
     # data = QuizAttemptData.where(:user_id=>current_user.id)
     if data.present?
-      s = {}
       data.each do |d|
         quiz = Quiz.where(:guid=>d.data["asset_download_id"]).last
+        s = {}
+        d = {}
         s["attemptId"] = d._id.to_s
         s["attemptedAt"] = d.data["start_time"]
         s["assessmentType"] = d.data["player_subtype"]
         s["assessmentName"] = quiz.name rescue ""
         s["assessmentGuid"] = d.data["asset_download_id"]
         s["uri_path"] = TocData.where(:downloadId=>d.data["asset_download_id"]).last.path rescue ""
-        result << s
+        attempts << s
+        if !a.keys.include? d.data["asset_download_id"]
+          a[d.data["asset_download_id"]] = d.data["asset_download_id"]
+          d["assessmentGuid"] = d.data["asset_download_id"]
+          d["uri_path"] = s["uri_path"]
+          assessments  <<  d
+        end
       end
     end
+    result = {:assessments=>assessments,:attempts=>attempts}
     render json: result
   end
 
