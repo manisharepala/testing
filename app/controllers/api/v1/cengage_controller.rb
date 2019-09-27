c5a57e70de928df2e4b61a00913d484163d3e201
inayat <peace.inayat@gmail.com>
2019-09-27 11:32:54 +0530

    imps

--- app/controllers/api/v1/cengage_controller.rb
+++ app/controllers/api/v1/cengage_controller.rb
@@ -255,7 +255,7 @@ class Api::V1::CengageController < ApplicationController
       quiz.tags_verified = true
       quiz.save!
 
-      data = {'success'=>true,'asset_download_id'=>quiz.id,'assessment_guid'=>quiz.id,'test_download_id'=>quiz.id}
+      data = {'success'=>true,'asset_download_id'=>quiz.guid,'assessment_guid'=>quiz.id,'test_download_id'=>quiz.id}
     end
 
     render json: data
