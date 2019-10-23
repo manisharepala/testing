b9793e719f3b8f50a49398f8e2445c11e926726d
dilip <dilip.bv@myedutor.com>
2019-10-23 14:05:10 +0530

    get_user_quiz_attempt_rank

--- app/models/quiz_attempt_data.rb
+++ app/models/quiz_attempt_data.rb
@@ -447,9 +447,11 @@ class QuizAttemptData
     data = []
     assessments.each do |assessment|
       quiz = Quiz.where(:guid=>assessment)
-      attempt = QuizAttempt.where(:quiz_guid=>assessment).last
+      attempt = QuizAttempt.where(:quiz_guid=>assessment,:user_id=>user_id).first
+      if attempt.presnt?
       data <<  {"name"=>quiz.name,"score"=>attempt.marks_scored,"date"=>Time.at(attempt.end_time),"rank"=>get_user_quiz_attempt_rank(assessment,user_id,attempt.quiz_attempt_data_id),
                 "subject_data" => get_quiz_section_data(assessment,user_id,attempt.quiz_attempt_data_id).map{|i| {"sub"=>i["sub"],"rank"=>i["rank"],"marks"=>i["marks"],"total_questions"=>i["total_questions"]}}}
+      end
     end
     return data
   end
