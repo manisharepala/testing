09ae8766af6c6619e7727d76b45adf8dbffdebbd
dilip <dilip.bv@myedutor.com>
2019-11-19 18:03:46 +0530

    application.rb updated

--- config/application.rb
+++ config/application.rb
@@ -44,12 +44,16 @@ module AssessmentApp
 
     config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
 
-    # config.before_configuration do
-    #   env_file = File.join(Rails.root, 'config', 'local_env.yml')
-    #   YAML.load(File.open(env_file)).each do |key, value|
-    #     ENV[key.to_s] = value
-    #   end if File.exists?(env_file)
-    # end
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        if key.to_s==Rails.env
+          value.each do |k,v|
+            ENV[k.to_s] = v
+          end
+        end
+      end if File.exists?(env_file)
+    end
 
     options = {
         host: ENV['REDIS_SERVER'],
